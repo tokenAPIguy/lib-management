2a7b66ce73043fd00c8a76888eba46054eefda75
Jake Levy <77429039+sushi-tears@users.noreply.github.com>
2025-02-01 14:32:41 -0500

    adding endpoints

--- LBMS_API/Program.cs
+++ LBMS_API/Program.cs
@@ -1,10 +1,9 @@
 using LBMS_API.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -13,27 +12,22 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[] {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/api/v1", () => "Hello World!");
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapGet("/api/v1/categories", async (ApplicationDbContext db) => {
+    var categories = await db.Categories.ToListAsync();
+    return Results.Ok(categories);
+});
+
+app.MapGet("/api/v1/categories/{category}", async (string category, ApplicationDbContext db) => {
+    var categories = await db.Categories.Where(c => c.Name == category).ToListAsync();
+    return Results.Ok(categories);
+});
 
 app.Run();
 
